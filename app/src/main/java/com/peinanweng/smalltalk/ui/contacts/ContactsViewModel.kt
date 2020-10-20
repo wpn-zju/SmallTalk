ee2d491d40afe0c375d5c9d81973d6b0870aea9d
P. N. Weng <3150102239@zju.edu.cn>
2020-10-20 18:53:20 -0400

    Initial Commit

---
+++ app/src/main/java/com/peinanweng/smalltalk/ui/contacts/ContactsViewModel.kt
@@ -0,0 +1,13 @@
+package com.peinanweng.smalltalk.ui.contacts
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ContactsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
